a8424d1ad8c609a1bf77d140749c7b0947ff78d7
karlsjohnson <karl.s.johnson@gmail.com>
2021-06-21 18:57:28 -0700

    Update

--- root/initMM.sh
+++ root/initMM.sh
@@ -2,3 +2,8 @@
 
 alias cdhdd="cd /Volumes/HDD"
 alias cdssd="cd /Volumes/SSD"
+
+function restartSSH {
+  sudo launchctl unload /System/Library/LaunchDaemons/ssh.plist
+  sudo launchctl load -w /System/Library/LaunchDaemons/ssh.plist
+}
