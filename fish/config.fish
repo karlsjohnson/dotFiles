b8831a157b13aa5d890576d9957de044f7e3da02
Karl Johson <karl.s.johnson@gmail.com>
2021-11-13 16:53:54 -0700

    Update

--- fish/config.fish
+++ fish/config.fish
@@ -4,10 +4,9 @@ end
 # set fish config directory
 #----------  Source Files -----------
 source $HOME/.config/fish/custom/functions.fish
-source $HOME/.config/fish/custom/env.fish
-source $HOME/.config/fish/custom/alias.fish
-source $HOME/.config/fish/custom/private.fish
-source $HOME/.config/fish/custom/settings.fish
+source_custom env.fish
+source_custom alias.fish
+source_custom settings.fish
 source $Private/private.fish
 
 #----------  Config Done -----------
