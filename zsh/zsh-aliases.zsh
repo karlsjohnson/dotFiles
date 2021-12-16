de2d6b1795cae2c774940ee03431e2069b5bb378
Karl Johson <karl.s.johnson@gmail.com>
2021-12-16 10:07:02 -0700

    git alaises

--- zsh/zsh-aliases.zsh
+++ zsh/zsh-aliases.zsh
@@ -94,16 +94,16 @@ alias tmkss="tmux kill-session"
 # Git
 alias gi="git"
 alias gis="git status"
-alias gic="git commit -m \""
-alias gia="git add *"
-alias gih="git push"
+alias gic="git commit -a -m "
+alias gia="git add --all"
+alias gih="git push origin"
 alias gil="git pull"
 alias gicl="git clone"
 
 # Complete Git Add,Commit,Push
 function gich {
-  git add *
-  git commit -m "$1"
+  git add --all
+  git commit -a -m "$1"
   git push
   git status
 }
